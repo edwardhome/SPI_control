bd48f784491e503af973a6eadd818dde1c4341e9
edwardhome <edward61221@gmail.com>
2021-09-15 13:56:26 +0800

    新增ui介面 並且新增許多功能

--- rasp_gui.py
+++ rasp_gui.py
@@ -1,29 +1,38 @@
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox
+from PySide2 import QtCore
 from PySide2.QtCore import Qt
-
+from PySide2.QtUiTools import QUiLoader
+import os
 # export DISPLAY=':0.0' 利用該指令遠端ssh開啟gui程式
-
-def handleCalc():
-    QMessageBox.about(window,"Message","Hello Raspberry Pi")
-
-def stop():
-    app.quit()
-
-app = QApplication([]) #建立應用程式
-window = QMainWindow() #建立視窗物件
-window.resize(800,480) #確定視窗大小
-window.move(0,0) #開啟左上角在螢幕的位置
-window.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
-
-button = QPushButton("Test",window)
-button.move(100,100)
-button.resize(100,100)
-button.clicked.connect(handleCalc)
-
-button_stop = QPushButton("stop",window)
-button_stop.move(100,250)
-button_stop.resize(100,100)
-button_stop.clicked.connect(stop)
-
-window.show()
+class Stats(QtCore.QObject): #該類別必須為QObject，才可以調用sender
+    def __init__(self):
+        super().__init__()
+        self.ui =QUiLoader().load("main.ui")        
+        self.ui.setWindowFlags(Qt.Window | Qt.FramelessWindowHint) #使其無邊框
+        self.ui.resize(800,480)
+        self.ui.move(0,0)
+        self.ui.stop.clicked.connect(self.stop)
+        self.ui.open.clicked.connect(self.open)
+        self.ui.shutdown.clicked.connect(self.shutdown)
+    def stop(self):
+        app.quit()
+    def open(self):
+        pass
+    def close(self):
+        pass
+    def add(self):
+        pass
+    def sub(self):
+        pass
+    def shutdown(self):
+        qm = QMessageBox.question(self.ui,"您確定要關機","確定嗎？",QMessageBox.Yes |QMessageBox.No, QMessageBox.Yes)
+        if qm == QMessageBox.Yes:
+            os.system("shutdown -t 60")
+        else:
+            pass
+app = QApplication([])
+window = Stats()
+window.ui.show()
 app.exec_()
+
+
